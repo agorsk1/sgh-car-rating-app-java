f0db0e17fd8518ca375f201eeb1ee0062b3f63b2
artur-gorski-tisa <a.gorski@tisagroup.ch>
2022-09-23 17:23:24 +0200

    add serializable to vpc response

--- src/main/java/com/example/demo/car/VPCResponse.java
+++ src/main/java/com/example/demo/car/VPCResponse.java
@@ -3,10 +3,11 @@ package com.example.demo.car;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.io.Serializable;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class VPCResponse {
+public class VPCResponse implements Serializable {
     @JsonProperty("Results")
     private List<CarVPC> result;
 
